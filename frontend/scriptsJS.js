af5a508c2e13bdee75677f42408f3bddc56c3995
Srikumar-A <arivlagansrikumar@gmail.com>
2026-03-07 23:43:35 +0530

    Completed basic OAuth for now, retrieving emails done, working on parsing and classifying the emails, expect to complete it by tmrw session 1, some frontend stuff should get the shippable application ready for now, Version 2 has to store the job proceedings in backend for each user, timestamp based retreiver, and finally deploy the whole thing, marks the near completion of the project, will add few more features to it with time.

---
+++ frontend/scriptsJS.js
@@ -0,0 +1,52 @@
+import {CONFIG} from "./config.js";
+
+//------------------Global Variables------------------//
+let GOOGLE_CLIENT_ID=CONFIG.GOOGLE_CLIENT_ID;
+let API_BASE_URL=CONFIG.API_BASE_URL;
+
+//------------------User context------------------//
+// and eventlistner for google login
+document.getElementById("google-login-btn").addEventListener("click",()=>{
+    const redirect_uri="http://localhost:5500/frontend/callback.html";
+    const scope="https://www.googleapis.com/auth/gmail.readonly";
+    const authUrl = "https://accounts.google.com/o/oauth2/v2/auth" +
+                `?client_id=${GOOGLE_CLIENT_ID}` +
+                `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
+                `&response_type=token` +
+                `&scope=${encodeURIComponent(scope)}`;
+    const popup = window.open(authUrl, "Google Login", "width=500,height=600");
+
+    window.addEventListener("message", (event)=> {
+        if (event.data.type=="google-auth"){
+            const token=event.data.token;
+            //display the dashboard and hide login button
+            document.getElementById("login-screen").style.display="none";
+            //document.getElementById("dashboard").style.display="block";
+            sessionStorage.setItem("user",token);
+            console.log(token);
+            fetchEmails(token);
+        };
+    });
+    async function fetchUserInfo(token){
+        const res=await fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
+            headers:{ Authorization:`Bearer ${token}`}
+        });
+        const data=await res.json();
+        
+
+
+        
+    }
+
+});
+
+//------------------State------------------//
+async function fetchEmails(token){
+    const res=await fetch(`${API_BASE_URL}get-email-list`,{
+        headers:{ Authorization:`Bearer ${token}`}
+    });
+    const data=await res.json();
+    console.log(data);
+}
+
+//------------------Functions------------------//
--- server.py
+++ server.py
@@ -1,37 +1,51 @@
 import os
 from dotenv import load_dotenv
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import requests
 from google_auth_oauthlib.flow import InstalledAppFlow
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+import re # regex for parsing email content
 
 load_dotenv()
 base_url = os.getenv("BASE_URL")
 userId='me'
-SCOPES=["https://www.googleapis.com/auth/gmail.readonly"]
 
 app=FastAPI()
 
+
+# Adding middleware to handle CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (you can specify specific origins if needed)
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
 @app.get("/get-email-list")
-async def get_email_list():
-    response=requests.get(base_url + "/gmail/v1/users/{userId}/messages")
+async def get_email_list(request:Request):
+    token=request.headers.get("Authorization")
+    response=requests.get(base_url + "/gmail/v1/users/me/messages",
+                          headers={"Authorization": token})
+    return response.json()
+
+@app.get("/job-applications")
+async def get_job_applications(request:Request):
+    token=request.headers.get("Authorization")
+    response=requests.get(base_url + "/gmail/v1/users/me/messages",
+                          headers={"Authorization": token})
+    for i in range(len(response.json().get("messages", []))):
+        msg_id=response.json().get("messages", [])[i].get("id")
+        msg_response=requests.get(base_url + f"/gmail/v1/users/me/messages/{msg_id}",
+                                  headers={"Authorization": token})
+        
+        # if subject contains keywords
+
+        #else regex through the body to find job application
     return response.json()
 
 
 if __name__=="__main__":
-    flow=InstalledAppFlow.from_client_secrets_file(
-        {
-            "installed":{
-                "client_id":os.getenv("CLIENT_ID"),
-                "client_secret":os.getenv("CLIENT_SECRET"),
-                "redirect_uris":["http://localhost"],
-                "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-                "token_uri":"https://oauth2.googleapis.com/token"
-            }
-        },
-        scopes=SCOPES
-    )
-    creds=flow.run_local_server(port=0)
-    print("Access Token:", creds.token)
-    print("Refresh Token:", creds.refresh_token)
-    print("authenticated ...")
+    uvicorn.run("server:app",host="127.0.0.1",port=8000,reload=True)
